7f3490d5cdcace23eb90f5bf65a1a7e0e24c62e3
MinyeongKim <32475904+MinyeongKim@users.noreply.github.com>
2017-12-20 23:07:23 +0900

    Update video.php

--- final/video.php
+++ final/video.php
@@ -65,26 +65,27 @@
 	</head>
 
 	<body>
+		<!-- the top of page -->
 		<div id="main">
 			<a class="p1" align="left"><img src="logo_icon.png" height=35px vspace='50'></a>
-
-<div id="menu">
-<ul>
-				<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
-</ul>
-</div>
-
-<div id = "search">
+			<!-- menu -->
+			<div id="menu">
+				<ul>
+					<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
+				</ul>
+			</div>
+			<!-- search bar -->
+			<div id = "search">
 				<form method = "get" action ="search.php">
 					<table border="0" align="right">
 						<tr>
 							<td align ="center">
 								<select name="ser">
-								<option value="composer">Composer</option>
-								<option value="title">Title</option>
+									<option value="composer">Composer</option>
+									<option value="title">Title</option>
 								</select>
 							</td>
 							<td>
@@ -97,43 +98,35 @@
 					</table>
 				</form>
 			</div>
-
-</div>
-<br>
-	<div id = "body">
-	<!-- ·Î±×ÀÎÀ» ¾ÈÇßÀ» ½Ã, login.php·Î ÀÌµ¿½ÃÄÑÁØ´Ù. -->
-<?php 
-			if(!($_SESSION['login_email'])) {
-				echo ("<script>location.replace('login.php');</script>");
-			}
-			$mysql_hostname = 'localhost';
-			$mysql_username = 'root';
-			$mysql_password = 'apmsetup';
-			$mysql_database = 'musiccast';
-
-			// DB ¿¬°á
-			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
-			
-			// DB ¼±ÅÃ
-			mysql_select_db($mysql_database, $connect) or die('DB ¼±ÅÃ ½ÇÆÐ');
-
-			$nick = $_SESSION['login_nick'];
-			
-			$sql_find = "select v_num, v_file from h_video where nickname = '$nick' and v_check = 1";
-			$video_number = mysql_query($sql_find, $connect);
-			if($row = mysql_fetch_array($video_number)){
-
-			$video_num = $row["v_num"];
-			$video_title = $row["v_file"];
-			unlink($video_title);
-			$sql_delete = "delete from h_video where v_num = '$video_num'";
-			mysql_query($sql_delete, $connect);
-			}
-
-
-			mysql_close($connect);
-
-		?>
+		</div>
+		<br>
+		<div id = "body">
+			<!-- check login state and video -->
+			<?php 
+				// if user is not login state, go login page
+				if(!($_SESSION['login_email'])) {
+					echo ("<script>location.replace('login.php');</script>");
+				}
+				$mysql_hostname = 'localhost';
+				$mysql_username = 'root';
+				$mysql_password = 'apmsetup';
+				$mysql_database = 'musiccast';
+				// connect DB
+				$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+				// select DB
+				mysql_select_db($mysql_database, $connect) or die('Fail DB select');
+				// check uploaded video file by user
+				$nick = $_SESSION['login_nick'];
+				$sql_find = "select v_num from h_video where nickname = '$nick' and v_check = 1";
+				$video_number = mysql_query($sql_find, $connect);
+				// if there is video not to be finished to upload, delete that
+				$row = mysql_fetch_array($video_number);
+				$video_num = $row["v_num"];
+				$sql_delete = "delete from h_video where v_num = '$video_num'";
+				mysql_query($sql_delete, $connect);
+				// close DB
+				mysql_close($connect);
+			?>
 	<div id = "main_div1">
 		<div id ="main_sub_div1">
 		<?php 
@@ -142,10 +135,10 @@
 			$mysql_password = 'apmsetup';
 			$mysql_database = 'musiccast';
 
-			// DB ¿¬°á
+			// DB ì—°ê²°
 			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
 
-			// DB ¼±ÅÃ
+			// DB ì„ íƒ
 			mysql_select_db($mysql_database, $connect) or die('Fail DB selection');
 			$composer = $_SESSION['login_composer'];
 				$nick = $_SESSION['login_nick'];
