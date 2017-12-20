103b398f5707a3edcc3a2c0b715adb9213533488
eom012 <33171438+eom012@users.noreply.github.com>
2017-12-20 23:32:07 +0900

    Update logout.php

--- final/logout.php
+++ final/logout.php
@@ -4,15 +4,15 @@
       <style type="text/css">
          @import url(main.css);
          @font-face {
-            font-family : '경필명조B';
-            src : url('resources/fonts/DX경필명조B.ttf');
+            font-family : '寃쏀븘紐낆“B';
+            src : url('resources/fonts/DX寃쏀븘紐낆“B.ttf');
          }
          body
          {
             background-image : url("background.png");
             background-repeat : no-repeat;
             background-size : 100%;
-            font-family : '경필명조B';
+            font-family : '寃쏀븘紐낆“B';
             color : white;
          }
          #top_margin {
@@ -36,38 +36,40 @@
 
       </style>
    </head>
+	<body>
+		<?php
+			$mysql_username = 'root';
+			$mysql_password = 'apmsetup';
+			$mysql_database = 'musiccast';
+			session_start();
 
-<?php
-   $mysql_username = 'root';
-   $mysql_password = 'apmsetup';
-   $mysql_database = 'musiccast';
-   //php에서 session을 이용하기 위해서는 session_start()를 무조건 해줘야함
-   session_start();
-   // DB 연결
-   $connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+			// connect DB
+			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
 
-   // DB 선택
-   mysql_select_db($mysql_database, $connect) or die('Fail DB selection');
+			// select DB
+			mysql_select_db($mysql_database, $connect) or die('Fail DB selection');
 
-   //$login_sql = "UPDATE user_info SET plag = 0 WHERE email = '$_SESSION['login_email']'";
-   //mysql_query($login_sql, $connect);
-   //session으로 로그인을 유지했기 때문에 이를 없애주는 것
-   session_destroy();
-  ?> 
-   <div id = "top_margin">
-   <p></p>
-   </div>
-   <div id = "login_margin_left">
-   <p></p>
-   </div>
-
-   <div id = "login_logo">
-    <center><image src = "logo_icon.png" width = 60%>
-	</div>
-	<div id = "login">
-	<center><table> <tr> <td align="center">Login completed successfully.</td></tr>
-	<tr><td align="center">If you want to login, <a href = "login.php">Click</a> Please.</td></tr></table></div>
-
-
-
+			// destroy session for logout
+			session_destroy();
+		?> 
+   		<div id = "top_margin">
+   			<p></p>
+   		</div>
+   		<div id = "login_margin_left">
+   			<p></p>
+   		</div>
+   		<div id = "login_logo">
+    			<center><image src = "logo_icon.png" width = 60%>
+		</div>
+		<div id = "login">
+			<center><table> 
+				<tr> 
+					<td align="center">Login completed successfully.</td>
+				</tr>
+				<tr>
+					<td align="center">If you want to login, <a href = "login.php">Click</a> Please.</td>
+				</tr>
+			</table>
+		</div>
+	</body>
 </html>
