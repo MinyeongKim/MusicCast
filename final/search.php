dd082c2a91c2a8217e5592e184ca967c9e328f91
MinyeongKim <32475904+MinyeongKim@users.noreply.github.com>
2017-12-20 23:34:48 +0900

    Update search.php

--- final/search.php
+++ final/search.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <html lang="ko">
 	<head> 
-		<title>나린아토</title>
+		<title>NARINATO</title>
 		<link rel="stylesheet" href="style.css"/>
 		<style type="text/css">
 			@import url(main.css);
@@ -48,7 +48,7 @@
 </ul>
 </div>
 
-<!-- DB연동해서 검색하는거 구현 해야됨 -->
+<!-- search bar -->
 <div id = "search">
 				<form method = "get" action ="search.php">
 					<table border="0" align="right">
@@ -72,8 +72,9 @@
 </div>
 <br>
 	<div id = "body">
-	<!-- 로그인을 안했을 시, login.php로 이동시켜준다. -->
+	<!-- check login state and video -->
 	<?php 
+			//if user is not login state, go login page
 			if(!($_SESSION['login_email'])) {
 				echo ("<script>location.replace('login.php');</script>");
 			}
@@ -82,19 +83,24 @@
 			$mysql_password = 'apmsetup';
 			$mysql_database = 'musiccast';
 
-			// DB 연결
+			// DB connect
 			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
 			
-			// DB 선택
-			mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+			// DB select
+			mysql_select_db($mysql_database, $connect) or die('Fail DB select');
 
+			// check uploaded video file by user
 			$nick = $_SESSION['login_nick'];
 			$sql_find = "select v_num from h_video where nickname = '$nick' and v_check = 1";
 			$video_number = mysql_query($sql_find, $connect);
+		
+			// if there is video not to be finished to upload, delete that
 			$row = mysql_fetch_array($video_number);
 			$video_num = $row["v_num"];
 			$sql_delete = "delete from h_video where v_num = '$video_num'";
 			mysql_query($sql_delete, $connect);
+			
+			// DB close
 			mysql_close($connect);
 
 		?>
@@ -106,15 +112,17 @@
 				$mysql_password = 'apmsetup';
 				$mysql_database = 'musiccast';
 
-				// DB 연결
+				// DB connect
 				$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
 
-				// DB 선택
+				// DB select
 				mysql_select_db($mysql_database, $connect) or die('Fail DB selection');
 				
 				$composer = $_SESSION['login_composer'];
 				$nick = $_SESSION['login_nick'];
-
+				
+			
+				// check if user is composer
 				if ($composer == 1) {
 					$sql_composer = "select * from composer where nickname = '$nick'";
 					$video_number = mysql_query($sql_composer, $connect);
@@ -138,18 +146,18 @@ $mysql_username = 'root';
 $mysql_password = 'apmsetup';
 $mysql_database = 'musiccast';
 
-// DB 연결
+// DB connect
 $connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password);
-// DB 선택
-mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+// DB select
+mysql_select_db($mysql_database, $connect) or die('Fail DB select');
 
 	$bno = $_GET["indx"];
 	$sql = "select * from h_video where v_num='".$bno."'";
 
 	$result = mysql_query($sql, $connect);
 	$board = mysql_fetch_array($result);
-/* 조회수 카운트 */
-
+	
+	// Add views of video when user watch it
 	$sql_hit = "select * from h_video where v_num ='".$bno."'";
 	$result_hit = mysql_query($sql_hit,$connect);
 	$hit = mysql_fetch_array($result_hit);
@@ -160,7 +168,8 @@ mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
 	<div id = "main_div2">
 		<?php
 			$cate=$_GET['ser']; 
-			$text=$_GET['keyWord']; 
+			$text=$_GET['keyWord'];
+			// When search category is 'composer', print composer with search content
 			if (strcmp($cate, 'composer') == 0) {
 				$sql = "select * from composer where nickname like '%$text%'";
 		?>
@@ -176,8 +185,10 @@ mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
 					</tbody>
 					<?php } ?>
 			</table>
-					<?php
-					}
+			<?php
+			}
+			
+			// When search category is 'title', print video with search content
 			else if (strcmp($cate, 'title') == 0) {
 				$sql = "select * from h_video where title like '%$text%'";
 				?>
