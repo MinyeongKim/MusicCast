59b831506642cde0d710de0d2edc2be62321a353
eom012 <33171438+eom012@users.noreply.github.com>
2017-12-20 23:36:49 +0900

    Update modify.php

--- final/modify.php
+++ final/modify.php
@@ -13,9 +13,9 @@
 			
 			#main_sub_div1
 			{
-            width : 200px;
-            height : 70px;
-            margin: 10px 10px 10px 40px;
+            			width : 200px;
+            			height : 70px;
+            			margin: 10px 10px 10px 40px;
 			}
 			table, th, td
 			{
@@ -30,27 +30,27 @@
 	</head>
 
 	<body>
+		<!-- the top of page -->
 		<div id="main">
 			<a class="p1" align="left"><img src="logo_icon.png" height=35px vspace='50'></a>
-
-<div id="menu">
-<ul>
-				<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
-</ul>
-</div>
-
-<!-- DB연동해서 검색하는거 구현 해야됨 -->
+			<!-- menu -->
+			<div id="menu">
+				<ul>
+					<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
+				</ul>
+			</div>
+			<!-- search bar -->
 			<div id = "search">
 				<form method = "get" action ="search.php">
 					<table border="0" align="right">
 						<tr>
 							<td align ="center">
 								<select name="ser">
-								<option value="composer">Composer</option>
-								<option value="title">Title</option>
+									<option value="composer">Composer</option>
+									<option value="title">Title</option>
 								</select>
 							</td>
 							<td>
@@ -63,57 +63,72 @@
 					</table>
 				</form>
 			</div>
-</div>
-<br>
-	<div id = "body">
-	<!-- 로그인을 안했을 시, login.php로 이동시켜준다. -->
-	<?php 
-		if(!($_SESSION['login_email'])) {
-			echo ("<script>location.replace('login.php');</script>");
-		}
-	?>
-	<div id = "main_div1">
-		<div id ="main_sub_div1">
-		<?php 
-			$mysql_hostname = 'localhost';
-			$mysql_username = 'root';
-			$mysql_password = 'apmsetup';
-			$mysql_database = 'musiccast';
-
-			// DB 연결
-			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
-
-			// DB 선택
-			mysql_select_db($mysql_database, $connect) or die('Fail DB seletion');
-			$composer = $_SESSION['login_composer'];
-				$nick = $_SESSION['login_nick'];
-
-				if ($composer == 1) {
-					$sql_composer = "select * from composer where nickname = '$nick'";
-					$video_number = mysql_query($sql_composer, $connect);
-					if($row = mysql_fetch_array($video_number)){
-						echo "<a href = \"$row[h_address]\">COMPOSER</a>";
-					}
-				}
-				else
-					echo "USER";
-			echo "<br><br>Welcome ". $_SESSION['login_nick'] ."";
-			echo "<center><input type = \"button\" value = \"Logout\" onclick = \"location.href = 'logout.php'\">";
-			echo "<div id=\"menu\">
-			<ul>
-				<li><a href=\"DAMIN_composer.php\">VIDEO</a></li>
-				<li><a href=\"timeline.php\">TIMELINE</a></li>
-			</ul>
-			</div>";
-			$bno = $_GET["indx"];
-		?>
 		</div>
-	</div>
-	<div id = "main_div2" align="center">
+		<br>
+		<div id = "body">
+			<!-- check login state and video -->
+			<?php 
+				// if user is not login state, go login page
+				if(!($_SESSION['login_email'])) {
+					echo ("<script>location.replace('login.php');</script>");
+				}
+				$mysql_hostname = 'localhost';
+				$mysql_username = 'root';
+				$mysql_password = 'apmsetup';
+				$mysql_database = 'musiccast';
+				// connect DB
+				$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+				// select DB
+				mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+				// check uploaded video file by user
+				$nick = $_SESSION['login_nick'];
+				$sql_find = "select v_num from h_video where nickname = '$nick' and v_check = 1";
+				$video_number = mysql_query($sql_find, $connect);
+				// if there is video not to be finished to upload, delete that
+				$row = mysql_fetch_array($video_number);
+				$video_num = $row["v_num"];
+				$sql_delete = "delete from h_video where v_num = '$video_num'";
+				mysql_query($sql_delete, $connect);
+				// close DB
+				mysql_close($connect);
+			?>
+		
+			<div id = "main_div1">
+				<div id ="main_sub_div1">
+					<?php 
+						$mysql_hostname = 'localhost';
+						$mysql_username = 'root';
+						$mysql_password = 'apmsetup';
+						$mysql_database = 'musiccast';
+						// connect DB
+						$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+						// select DB
+						mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+						$composer = $_SESSION['login_composer'];
+						$composer = $_SESSION['login_composer'];
+						$nick = $_SESSION['login_nick'];
+						// check user is composer or user, if composer, link the page.
+						if ($composer == 1) {
+							$sql_composer = "select * from composer where nickname = '$nick'";
+							$video_number = mysql_query($sql_composer, $connect);
+							if($row = mysql_fetch_array($video_number)){
+								echo "<a href = \"$row[h_address]\">COMPOSER</a>";
+							}
+						}
+						else
+							echo "USER";
+					?>
+					<?php echo "<br>Welcome ". $_SESSION['login_nick'] ." <br>"; ?>
+					<!-- logout button -->
+					<center><input type = "button" value = "Logout" onclick = "location.href = 'logout.php'">
+				</div>
+			</div>
 			
-			<div>
-				<form action="modify_ok.php?indx=<?php echo $bno; ?>" method="post">
+			<div id = "main_div2" align="center">
+				<div>
+					<form action="modify_ok.php?indx=<?php echo $bno; ?>" method="post">
 						<table>
+							<tr>
 								<td><label>Title</label></td>
 								<td height="30"><input type="text" name="title" size="50"/></td>
 							</tr>
@@ -144,10 +159,10 @@
 								<td align="right"colspan="2"><input type="submit" value="Submit"></td>
 							</tr>
 						</table>
-				</form>
-        </div>
-	</div>
-</body>
+					</form>
+        		</div>
+		</div>
+	</body>
 </html>
 
 
