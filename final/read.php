100cdaeffe9db803e6dc906af0ae34ef6826e0e3
eom012 <33171438+eom012@users.noreply.github.com>
2017-12-20 23:43:01 +0900

    Update read.php

--- final/read.php
+++ final/read.php
@@ -37,26 +37,27 @@
 	</head>
 
 	<body>
+		<!-- the top of page -->
 		<div id="main">
 			<a class="p1" align="left"><img src="logo_icon.png" height=35px vspace='50'></a>
-
-<div id="menu">
-<ul>
-				<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
-				<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
-</ul>
-</div>
-
-<div id = "search">
+			<!-- menu -->
+			<div id="menu">
+				<ul>
+					<li><a href="home.php"><img src="Home_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="ranking.php"><img src="ranking_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="video.php"><img src="video_icon.png" height=30px vspace='40'></a></li>
+					<li><a href="composer.php"><img src="composer_icon.png" height=30px vspace='40'></a></li>
+				</ul>
+			</div>
+			<!-- search bar -->
+			<div id = "search">
 				<form method = "get" action ="search.php">
 					<table border="0" align="right">
 						<tr>
 							<td align ="center">
 								<select name="ser">
-								<option value="composer">Composer</option>
-								<option value="title">Title</option>
+									<option value="composer">Composer</option>
+									<option value="title">Title</option>
 								</select>
 							</td>
 							<td>
@@ -69,139 +70,107 @@
 					</table>
 				</form>
 			</div>
-
-</div>
-<br>
-
-	<!-- 로그인을 안했을 시, login.php로 이동시켜준다. -->
-	<?php 
-			if(!($_SESSION['login_email'])) {
-				echo ("<script>location.replace('login.php');</script>");
-			}
-			$mysql_hostname = 'localhost';
-			$mysql_username = 'root';
-			$mysql_password = 'apmsetup';
-			$mysql_database = 'musiccast';
-
-			// DB 연결
-			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
-			
-			// DB 선택
-			mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
-
-			$nick = $_SESSION['login_nick'];
-			$sql_find = "select v_num from h_video where nickname = '$nick' and v_check = 1";
-			$video_number = mysql_query($sql_find, $connect);
-			$row = mysql_fetch_array($video_number);
-			$video_num = $row["v_num"];
-			$sql_delete = "delete from h_video where v_num = '$video_num'";
-			mysql_query($sql_delete, $connect);
-			mysql_close($connect);
-
-		?>
-	<div id = "main_div1">
-		<div id ="main_sub_div1">
-		<?php 
-			$mysql_hostname = 'localhost';
-			$mysql_username = 'root';
-			$mysql_password = 'apmsetup';
-			$mysql_database = 'musiccast';
-
-			// DB 연결
-			$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
-
-			// DB 선택
-			mysql_select_db($mysql_database, $connect) or die('Fail DB selection');
-			$composer = $_SESSION['login_composer'];
-			if ($composer == 1)
-				echo "COMPOSER";
-			else
-				echo "USER";
-			echo "<br><br>Welcome ". $_SESSION['login_nick'] ."<br><br>";
-			echo "<input type = \"button\" value = \"Logout\" onclick = \"location.href = 'logout.php'\">";
-			echo "<div id=\"menu\">
-			<ul>
-				<li><a href=\"DAMIN_composer.php\">VIDEO</a></li>
-				<li><a href=\"timeline.php\">TIMELINE</a></li>
-			</ul>
-			</div>";
-		?>
-	<center>
-	</div>
-	</div>
-	<?php
-//include "../db.php";
-
-$mysql_hostname = 'localhost';
-$mysql_username = 'root';
-$mysql_password = 'apmsetup';
-$mysql_database = 'musiccast';
-
-// DB 연결
-$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password);
-// DB 선택
-mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
-
-	$bno = $_GET["indx"];
-	$sql = "select * from h_video where v_num='".$bno."'";
-
-	$result = mysql_query($sql, $connect);
-	$board = mysql_fetch_array($result);
-/* 조회수 카운트 */
-
-	$sql_view = "select * from h_video where v_num ='".$bno."'";
-	$result_view = mysql_query($sql_view,$connect);
-	$view = mysql_fetch_array($result_view);
-	$view = $view['view'] + 1;
-	$fet = mysql_query("update h_video set view = '".$view."' where v_num = '".$bno."'", $connect);
-	?>
-
-	<div id = "main_div2">
-		<form action="timeline_write.php" method="post">
-                    <center><div id="board_read">
-					<table>
-						<tr>
-							<td class="read w10 fl">Title</td>
-							<td class="read_con" width = "150">&nbsp;<?php echo $board['title'];?></td>
-							<td class="read w5 fl">Index</td>
-							<td class="read_con" width = "150">&nbsp;<?php echo $board['v_num'];?></td>
-							<td rowspan = "2" class="read w10 fl">Writer</td>
-							<td rowspan = "2" class="read_con" width = "150">&nbsp;<?php echo $board['nickname'];?></td>
-							<td></td>
-						</tr>
-						<tr>
-							<td class="read w10 fl">Date</td>
-							<td class="read_con">&nbsp;<?php echo $board['upload_date'];?></td>
-							<td class="read w10 fl">Views</td>
-							<td class="read_con">&nbsp;<?php echo $board['view']+1;?></td>
-						</tr>
-						<tr>
-							<td class="read_nl fl">Context</td>
-							<td class="read_nl_con"><?php echo nl2br("$board[context]");  ?></td>
-							<td colspan = "3"><?php 
-							echo ("<video src={$board[v_file]} controls></video>"); ?> </td>
-							
-						</tr>
-						<tr>
-							<td width = "150"><a href="DAMIN_composer.php">[go to list]</a></td>
-						<?php
+		</div>
+		<br>
+		<div id = "body">
+			<!-- check login state and video -->
+			<?php 
+				// if user is not login state, go login page
+				if(!($_SESSION['login_email'])) {
+					echo ("<script>location.replace('login.php');</script>");
+				}
+				$mysql_hostname = 'localhost';
+				$mysql_username = 'root';
+				$mysql_password = 'apmsetup';
+				$mysql_database = 'musiccast';
+				// connect DB
+				$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+				// select DB
+				mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+				// check uploaded video file by user
+				$nick = $_SESSION['login_nick'];
+				$sql_find = "select v_num from h_video where nickname = '$nick' and v_check = 1";
+				$video_number = mysql_query($sql_find, $connect);
+				// if there is video not to be finished to upload, delete that
+				$row = mysql_fetch_array($video_number);
+				$video_num = $row["v_num"];
+				$sql_delete = "delete from h_video where v_num = '$video_num'";
+				mysql_query($sql_delete, $connect);
+				// close DB
+				mysql_close($connect);
+			?>
+		
+			<div id = "main_div1">
+				<div id ="main_sub_div1">
+					<?php 
+						$mysql_hostname = 'localhost';
+						$mysql_username = 'root';
+						$mysql_password = 'apmsetup';
+						$mysql_database = 'musiccast';
+						// connect DB
+						$connect = mysql_connect($mysql_hostname, $mysql_username, $mysql_password); 
+						// select DB
+						mysql_select_db($mysql_database, $connect) or die('DB 선택 실패');
+						$composer = $_SESSION['login_composer'];
+						$composer = $_SESSION['login_composer'];
 						$nick = $_SESSION['login_nick'];
-						if (strcmp($board['nickname'], $nick) == 0){
-							?>
-							<td width = "150"><a href="modify.php?indx=<?php echo $bno; ?>">[modify]</a></td>
-							<td width = "150"><a href="delete.php?indx=<?php echo $bno; ?>">[delete]</a></td>
-						<?php } ?>
-						</tr>
+						// check user is composer or user, if composer, link the page.
+						if ($composer == 1) {
+							$sql_composer = "select * from composer where nickname = '$nick'";
+							$video_number = mysql_query($sql_composer, $connect);
+							if($row = mysql_fetch_array($video_number)){
+								echo "<a href = \"$row[h_address]\">COMPOSER</a>";
+							}
+						}
+						else
+							echo "USER";
+					?>
+					<?php echo "<br>Welcome ". $_SESSION['login_nick'] ." <br>"; ?>
+					<!-- logout button -->
+					<center><input type = "button" value = "Logout" onclick = "location.href = 'logout.php'">
+				</div>
+			</div>
+			<div id = "main_div2">
+				<form action="timeline_write.php" method="post">
+                    			<center><div id="board_read">
+						<table>
+							<tr>
+								<td class="read w10 fl">Title</td>
+								<td class="read_con" width = "150">&nbsp;<?php echo $board['title'];?></td>
+								<td class="read w5 fl">Index</td>
+								<td class="read_con" width = "150">&nbsp;<?php echo $board['v_num'];?></td>
+								<td rowspan = "2" class="read w10 fl">Writer</td>
+								<td rowspan = "2" class="read_con" width = "150">&nbsp;<?php echo $board['nickname'];?></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td class="read w10 fl">Date</td>
+								<td class="read_con">&nbsp;<?php echo $board['upload_date'];?></td>
+								<td class="read w10 fl">Views</td>
+								<td class="read_con">&nbsp;<?php echo $board['view']+1;?></td>
+							</tr>
+							<tr>
+								<td class="read_nl fl">Context</td>
+								<td class="read_nl_con"><?php echo nl2br("$board[context]");  ?></td>
+								<td colspan = "3"><?php 
+								echo ("<video src={$board[v_file]} controls></video>"); ?> </td>
 
-					</table>
+							</tr>
+							<tr>
+								<td width = "150"><a href="DAMIN_composer.php">[go to list]</a></td>
+							<?php
+							$nick = $_SESSION['login_nick'];
+							if (strcmp($board['nickname'], $nick) == 0){
+								?>
+								<td width = "150"><a href="modify.php?indx=<?php echo $bno; ?>">[modify]</a></td>
+								<td width = "150"><a href="delete.php?indx=<?php echo $bno; ?>">[delete]</a></td>
+							<?php } ?>
+							</tr>
+
+						</table>
 					</div>
-					<div class="bo_ser">
-						<ul>
-							
-						</ul>
-					</div>
-            </form>
-	</div>
-</div>
-</body>
+            			</form>
+			</div>
+		</div>
+	</body>
 </html>
